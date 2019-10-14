d7ccbff6ac2098352fb2cc05e0061d281f838974
Omar Hassan <omarelessiely@outlook.com>
2019-10-14 22:46:13 +0200

    Add project files.

---
+++ DelegatesTraining/OperationManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesTraining
+{
+    public enum Operation
+    {
+        Sum,
+        Subtract,
+        Multiply
+    }
+
+    public class ExecutionManager
+    {
+        public  Dictionary<Operation,Func<dynamic>> FuncExecute { get; set; }
+        private Func<dynamic> _sum;
+        private Func<dynamic> _subtract;
+        private Func<dynamic> _multiply;
+
+        public ExecutionManager()
+        {
+            FuncExecute = new Dictionary<Operation, Func<dynamic>>(3);
+        }
+
+        public void PopulateFunctions(Func<dynamic> sum, Func<dynamic> subtract, Func<dynamic> multiply)
+        {
+            _subtract = subtract;
+            _sum = sum;
+            _multiply = multiply;
+        }
+
+        public void PrepareExecution()
+        {
+            FuncExecute.Add(Operation.Sum,_sum);
+            FuncExecute.Add(Operation.Multiply,_multiply);
+            FuncExecute.Add(Operation.Subtract,_subtract);
+        }
+    }
+
+    public class OperationManager
+    {
+        private int _first;
+        private int _second;
+        private readonly ExecutionManager _executionManager;
+
+        public OperationManager(int first, int second,ExecutionManager executionManager)
+        {
+            _first = first;
+            _second = second;
+            _executionManager = executionManager;
+            _executionManager.PopulateFunctions(Sum,Subtract,Multiply);
+            _executionManager.PrepareExecution();
+        }
+        private dynamic Sum()
+        {
+            return "Sum"+_first + _second;
+        }
+
+        private dynamic Subtract()
+        {
+            return _first - _second;
+        }
+
+        private dynamic Multiply()
+        {
+            return _first * _second;
+        }
+
+        public dynamic Execute(Operation operation)
+        {
+            return _executionManager.FuncExecute.ContainsKey(operation)
+                ? _executionManager.FuncExecute[operation]()
+                : -1;
+        }
+
+    }
+
+
+}
---
+++ DelegatesTraining/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesTraining
+{
+    class Program
+    {
+        //Declaration
+        public delegate void WriterDelegate(string text);
+
+        public static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            //Instantiation
+            WriterDelegate writerDelegate = new WriterDelegate(Write);
+
+            //Invocation 
+            writerDelegate("Welcome");
+
+            // Delegetes have 2 built in 
+            // 1- Func<T> takes up to 16 parameters and return value not void 
+
+            Func<int, int, int> sumDelegate = Sum;
+
+            Console.WriteLine(sumDelegate(10,20));
+            Console.WriteLine(sumDelegate(10,20));
+
+            // 2- Action<T> takes up to 16 parameters and not return any thing 
+
+            Action<string> writeDelegate = Write;
+
+            writeDelegate("String Action");
+
+
+
+            var executionManager = new ExecutionManager();
+            var opManager = new OperationManager(20,10,executionManager);
+            var result = opManager.Execute(Operation.Sum);
+            Console.WriteLine(result);
+            Console.WriteLine(opManager.Execute(Operation.Multiply));
+
+
+            Console.Read();
+
+        }
+    }
+}
